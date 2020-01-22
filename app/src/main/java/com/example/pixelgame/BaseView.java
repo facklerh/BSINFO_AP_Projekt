abaf79b2896e962fbeb52fbf0d7d952dc4d65b4e
Heiko Fackler <Heiko.Fackler@kratzer-automation.com>
2020-01-22 13:29:10 +0100

    you can now enter fullscreen mode via up swipe

--- app/src/main/java/com/example/pixelgame/BaseView.java
+++ app/src/main/java/com/example/pixelgame/BaseView.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
 public abstract class BaseView extends AppCompatActivity {
+
+    private float swipeStartPoint = 0f;
+    private float swipeEndPoint = 0f;
 
     // Lifecycle activities
 
@@ -19,13 +24,21 @@ public abstract class BaseView extends AppCompatActivity {
         setUI_fullscreen();
     }
 
-    private void setUI_fullscreen(){
+    private void setUI_fullscreen() {
+        hideDecorView();
+        hideActionBar();
+    }
+
+    private void hideDecorView() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             View decorView = getWindow().getDecorView();
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         }
+    }
+
+    private void hideActionBar() {
         ActionBar actionBar = getSupportActionBar();
-        if(actionBar!=null){
+        if (actionBar != null) {
             actionBar.hide();
         }
     }
@@ -37,15 +50,41 @@ public abstract class BaseView extends AppCompatActivity {
         AppData.save();
     }
 
+    // Listener
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                swipeStartPoint = event.getY();
+                Log.wtf("INFO", "ACTION_DOWN " + swipeStartPoint);
+                break;
+            case MotionEvent.ACTION_UP:
+                swipeEndPoint = event.getY();
+                Log.wtf("INFO", "ACTION_UP " + swipeEndPoint);
+                if (upSwipe()) {
+                    Log.wtf("INFO", "upswipe was recognized");
+                    setUI_fullscreen();
+                }
+
+        }
+        return true;
+    }
+
+    private boolean upSwipe() {
+        return swipeEndPoint < swipeStartPoint;
+    }
+
+
     // helpful Methods
 
-    public void launchActivity(Class cls){
+    public void launchActivity(Class cls) {
         Intent intent = new Intent(this, cls);
         startActivity(intent);
     }
 
-    public Button initNavigationButton(int btnID, final Class cls){
-        return initButton(btnID,new View.OnClickListener() {
+    public Button initNavigationButton(int btnID, final Class cls) {
+        return initButton(btnID, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 launchActivity(cls);
@@ -53,27 +92,27 @@ public abstract class BaseView extends AppCompatActivity {
         });
     }
 
-    public Button initButton(int btnID, View.OnClickListener onClick){
+    public Button initButton(int btnID, View.OnClickListener onClick) {
         Button button = findViewById(btnID);
-        if(button!=null){
+        if (button != null) {
             button.setOnClickListener(onClick);
         }
         return button;
     }
 
-    void showErrorDialog(){
+    void showErrorDialog() {
         showErrorDialog(R.string.errM_default);
     }
 
-    void showErrorDialog(int errorMessage){
-        showErrorDialog(R.string.error,errorMessage);
+    void showErrorDialog(int errorMessage) {
+        showErrorDialog(R.string.error, errorMessage);
     }
 
-    void showErrorDialog(int errorTitle, int errorMessage){
-        showErrorDialog(errorTitle,errorMessage,R.string.ok, null);
+    void showErrorDialog(int errorTitle, int errorMessage) {
+        showErrorDialog(errorTitle, errorMessage, R.string.ok, null);
     }
 
-    void showErrorDialog(int errorTitle, int errorMessage, int buttonMessage, DialogInterface.OnClickListener onClick){
+    void showErrorDialog(int errorTitle, int errorMessage, int buttonMessage, DialogInterface.OnClickListener onClick) {
         new AlertDialog.Builder(this)
                 .setTitle(errorTitle)
                 .setMessage(errorMessage)
@@ -83,11 +122,11 @@ public abstract class BaseView extends AppCompatActivity {
                 .show();
     }
 
-    void showExitDialog(int errorTitle, int errorMessage){
-        showErrorDialog(errorTitle,errorMessage,R.string.exit, exitOnClick());
+    void showExitDialog(int errorTitle, int errorMessage) {
+        showErrorDialog(errorTitle, errorMessage, R.string.exit, exitOnClick());
     }
 
-    DialogInterface.OnClickListener exitOnClick(){
+    DialogInterface.OnClickListener exitOnClick() {
         return new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
