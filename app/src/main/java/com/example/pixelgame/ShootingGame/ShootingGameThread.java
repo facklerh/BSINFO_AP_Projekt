d96b849311111ed1a9028bc0653949468f0fbcd4
Heiko Fackler <Heiko.Fackler@kratzer-automation.com>
2020-03-17 08:16:51 +0100

    update shooting game structure

--- app/src/main/java/com/example/pixelgame/ShootingGame/ShootingGameThread.java
+++ app/src/main/java/com/example/pixelgame/ShootingGame/ShootingGameThread.java
@@ -2,46 +2,65 @@ package com.example.pixelgame.ShootingGame;
 
 import android.util.Log;
 
-import java.io.OutputStream;
+import com.example.pixelgame.GameObject;
 
-public class GameThread extends Thread {
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShootingGameThread extends Thread {
 
     private final static long FPS = 30;
     public static final int SECOND_IN_MILLIS = 1000;
     private final static long MILLIS_PER_FRAME = SECOND_IN_MILLIS / FPS;
 
-    OutputStream output;
-
     boolean won = false;
     boolean lost = false;
+
+    GameObject player;
+    List<GameObject> ownShots;
+    List<GameObject> enemyShots;
+
+    OutputStream output;
+
+    public ShootingGameThread(OutputStream output, GameObject player) {
+        this.output = output;
+        this.player = player;
+
+        ownShots = new ArrayList<>(10);
+        enemyShots = new ArrayList<>(10);
+    }
 
     @Override
     public void run() {
         while (!won || !lost) {
             final long tickStart = System.currentTimeMillis();
             update();
+            render();
             final long tick = System.currentTimeMillis() - tickStart;
             addDelay(tick);
-            render();
-        }
-    }
-
-    private void addDelay(long tickStart) {
-        Log.i("Performance", SECOND_IN_MILLIS / tickStart + " fps");
-        if (tick < MILLIS_PER_FRAME) {
-            try {
-                Thread.sleep(MILLIS_PER_FRAME - tick);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
     }
 
     private void update() {
-
+        // TODO: update player position
+        // TODO: update
     }
 
     private void render() {
 
+    }
+
+    private void addDelay(long duration) {
+        Log.i("Performance", SECOND_IN_MILLIS / duration + " fps");
+        if (duration < MILLIS_PER_FRAME) {
+            try {
+                Thread.sleep(MILLIS_PER_FRAME - duration);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i("Performance", "performance bad! need improvement");
+        }
     }
 }
